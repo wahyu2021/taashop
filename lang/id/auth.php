a1b4435599df8d85014cecdd69ec56cad94e1956
wahyu2021 <wahyuwn256@gmail.com>
2026-05-23 11:58:47 +0700

    chore: publish language files and add Indonesian translations

---
+++ lang/id/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Identitas tersebut tidak cocok dengan data kami.',
+    'password' => 'Kata sandi yang dimasukkan salah.',
+    'throttle' => 'Terlalu banyak upaya masuk. Silakan coba lagi dalam :seconds detik.',
+];
